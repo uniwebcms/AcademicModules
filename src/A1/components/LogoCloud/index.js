10e7786f60d547c9d8103132c32214def21dd86c
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2025-04-07 23:51:40 +0800

    fix logo clouds

--- src/A1/components/LogoCloud/index.js
+++ src/A1/components/LogoCloud/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, stripTags, getPageProfile, Link } from '@uniwebcms/module-sdk';
+import { Image, stripTags, getPageProfile, Link, twJoin } from '@uniwebcms/module-sdk';
 import Container from '../_utils/Container';
 
 export default function LogoCloud({ block }) {
@@ -11,10 +11,12 @@ export default function LogoCloud({ block }) {
 
     return (
         <Container>
-            <div className='px-6 mx-auto max-w-7xl lg:px-8'>
-                <h2 className='text-3xl font-semibold text-center'>{stripTags(title)}</h2>
-                <h3 className='mt-2 text-xl font-medium text-center text-text-color-70'>{stripTags(subtitle)}</h3>
-                <div className='mt-8 md:mt-12 flex flex-wrap justify-center gap-0.5 lg:mt-8'>
+            <div className="px-6 mx-auto max-w-7xl lg:px-8">
+                <h2 className="text-3xl font-semibold text-center">{stripTags(title)}</h2>
+                <h3 className="mt-2 text-xl font-medium text-center text-text-color-70">
+                    {stripTags(subtitle)}
+                </h3>
+                <div className="mt-8 md:mt-12 flex flex-wrap justify-center gap-0.5 lg:mt-8">
                     {items.map(({ banner, links }, index) => {
                         const link = links[0];
 
@@ -25,14 +27,19 @@ export default function LogoCloud({ block }) {
                             <Wrapper
                                 key={index}
                                 {...wrapperProps}
-                                className='w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 flex justify-center p-6 transition-all ease-in-out bg-text-color-10 hover:bg-text-color-70'>
+                                className={twJoin(
+                                    'w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 h-20 flex items-center justify-center p-6 transition-all ease-in-out bg-text-color-10',
+                                    link ? 'hover:bg-text-color-70 cursor-pointer' : ''
+                                )}
+                            >
                                 {banner ? (
                                     <Image
                                         profile={getPageProfile()}
                                         value={banner.value}
                                         url={banner.url}
                                         alt={banner.alt}
-                                        className={`object-contain max-h-16 w-full`}></Image>
+                                        className={`object-contain max-h-16 w-full`}
+                                    ></Image>
                                 ) : null}
                             </Wrapper>
                         );
