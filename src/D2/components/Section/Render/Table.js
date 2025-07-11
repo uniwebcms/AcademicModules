c2e3a8389b6346a4036b54d4986e63ec8bc3cfab
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2025-07-11 22:48:52 +0800

    table styling

--- src/D2/components/Section/Render/Table.js
+++ src/D2/components/Section/Render/Table.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import Render from './index';
+import { twJoin } from '@uniwebcms/module-sdk';
 
 export default function Table(props) {
     const { content } = props;
 
     return (
-        <table className='border-collapse border border-text-color/50'>
-            <tbody className='divide-y divide-text-color/70'>
+        <table className="border-collapse border border-slate-200 dark:border-slate-700 divide-y divide-slate-200 dark:divide-slate-700">
+            <tbody className="divide-y">
                 {content.map((row, i) => {
                     const { content: rowContent } = row;
 
                     const tabRow = (
                         <tr key={i}>
                             {rowContent.map((cell, j) => {
-                                const { content: cellContent, attrs } = cell;
+                                const { type, content: cellContent, attrs } = cell;
 
                                 return (
                                     <td
                                         key={j}
                                         colSpan={attrs.colspan}
                                         rowSpan={attrs.rowspan}
-                                        className='text-left text-text-color border border-text-color/50 px-3 py-0 [&>p]:my-3 [&_*_p]:my-3'>
+                                        className={twJoin(
+                                            type === 'tableHeader'
+                                                ? 'bg-slate-100 dark:bg-slate-800 text-slate-900 dark:text-slate-200 font-semibold'
+                                                : 'bg-white dark:bg-slate-900 text-slate-700 dark:text-slate-300',
+                                            'text-left px-3 py-0 [&>p]:my-3 [&_*_p]:my-3',
+                                            'border border-slate-200 dark:border-slate-700'
+                                        )}
+                                    >
                                         <Render content={cellContent} />
                                     </td>
                                 );
