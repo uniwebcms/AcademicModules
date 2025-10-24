e51fd45886801635cf029b8a31889804ebce005a
tianyu <tianyu@proximify.ca>
2025-10-24 16:14:40 -0400

    Root level schemas;

--- src/D2/components/QuickLinks/index.js
+++ src/D2/components/QuickLinks/index.js
@@ -11,8 +11,8 @@ function QuickLink(props) {
     const elementProps = link ? { to: link.href } : {};
 
     return (
-        <div className="group relative rounded-xl border border-slate-200 dark:border-slate-700 dark:bg-slate-800/60">
-            <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]" />
+        <div className="group relative rounded-[var(--border-radius)] border border-[var(--border-color)] dark:border-[var(--border-dark-color)] dark:bg-slate-800/60">
+            <div className="absolute -inset-px rounded-[var(--border-radius)] border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]" />
             <div className="relative overflow-hidden rounded-xl p-6">
                 {icon ? (
                     <Icon icon={icon} className="h-8 w-8 text-sky-600" />
