c03a39969d26061cc08f4dccf463061f8a2fea2d
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2025-11-27 13:54:03 -0500

    A2 code

---
+++ src/A2/index.js
@@ -0,0 +1,21 @@
+// Import custom CSS
+import './index.css';
+
+// Import the components from the auto-generated dynamicExports.js file
+import * as dynamicExports from './dynamicExports';
+
+// Export selectable components under the module's "default" property
+export default dynamicExports;
+
+// Export site-level components and settings under the module's "site" property
+// Example:
+
+// export const site = {
+//     Layout: MyLayoutComponent       // take control of the site's layout
+//     props: { disableTheming: true, rootClassName: '' } // eg, disable auto theming
+//     report: { ... }                 // for document rendering (eg, Word/Excel/JSON)
+// };
+
+// export const site = {
+//     props: { rootClassName: 'min-h-screen flex flex-col justify-center context__light' },
+// };
---
+++ src/A2/tailwind.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+function makeEntryPath(libraryName, subpath = '**/*.{js,jsx,ts,tsx}') {
+    return path.join(path.dirname(require.resolve(libraryName)), subpath);
+}
+
+module.exports = {
+    content: ['../src/**/*.{js,jsx}', makeEntryPath('@uniwebcms/core-components')],
+    plugins: [require('@uniwebcms/uniweb-tailwind-plugin'), require('@tailwindcss/typography')],
+    theme: {
+        extend: {
+            typography: (theme) => ({
+                DEFAULT: {
+                    css: {
+                        color: 'inherit',
+                        a: {
+                            color: 'var(--link-color, inherit)',
+                            textDecoration: 'underline',
+                        },
+                        strong: {
+                            color: 'inherit',
+                            fontWeight: theme('fontWeight.bold'),
+                        },
+                        h1: {
+                            color: 'var(--heading-color, inherit)',
+                        },
+                        h2: {
+                            color: 'var(--heading-color, inherit)',
+                        },
+                        h3: {
+                            color: 'var(--heading-color, inherit)',
+                        },
+                        p: {
+                            color: 'var(--text-color, inherit)',
+                        },
+                        'h4, h5, h6, blockquote': {
+                            color: 'inherit',
+                        },
+                    },
+                },
+            }),
+        },
+    },
+};
